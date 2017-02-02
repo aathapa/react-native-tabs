d75b9a536127d4c620ab2b0cf3811e890e971a53
Aayush Thapa <aayush@Aayushs-MacBook-Pro.local>
2017-02-02 11:41:12 +0545

    Styles to Tab Bar

--- src/Routes.js
+++ src/Routes.js
@@ -4,11 +4,12 @@ import { Actions,Scene,Router } from 'react-native-router-flux';
 import Point from './components/Point';
 import FirstScreen from './components/FirstScreen';
 import Result from './components/Result';
+import Live from './components/Live';
 import TabIcon from './TabIcon';
 
 
 export default class AppRouter extends Component {
-    
+
     render() {
         return(
             <Router>
@@ -18,8 +19,9 @@ export default class AppRouter extends Component {
                         tabBarStyle={styles.tabBarStyle}
                         tabs
                     >
-                        <Scene key="homescreen" title="Points" component={Point} hideNavBar icon={TabIcon}  />
+                        <Scene key="homescreen" title="Points" component={Point} hideNavBar icon={TabIcon} />
                         <Scene key="tabView" title="Result" icon={TabIcon}  component={Result} hideNavBar/>
+                        <Scene key="live" title="Live" icon={TabIcon}  component={Live} hideNavBar/>
                     </Scene>
                     <Scene key="firstScreen" component={FirstScreen} hideNavBar initial/>
                 </Scene>
@@ -29,8 +31,8 @@ export default class AppRouter extends Component {
     }
 }
 const styles = {
-        tabBarStyle: {
-            top: 0,
-            backgroundColor: '#95a5a6'
-        }
+    tabBarStyle: {
+        top: 0,
+        backgroundColor: '#95a5a6'
     }
+}
--- src/TabIcon.js
+++ src/TabIcon.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import { Text,View } from 'react-native';
 
 const TabIcon = ({selected,title}) => {
+    const selectStyles = selected? styles.selectedTab:{}
     return(
         <View>
-            <Text style={{color: selected ? '#fff' :'#000'}}> {title}</Text> 
+            <Text style={[selectStyles,styles.tabText]}>{title}</Text> 
         </View>
            
     );
-        
+}
+const styles = {
+    selectedTab: {
+        fontSize: 18,
+        fontWeight: '700',
+        color: '#fff'
+    }
 }
 export default TabIcon;
